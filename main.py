d32e82e8192dbbd34f901305b1b0bef9dcf25080
Rushilsaxena07 <90499235+Rushilsaxena07@users.noreply.github.com>
2022-05-25 12:57:53 +0530

    made changes to main.py file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request #jsonify formats the output properly. ie, it returns a valid json
+from flask import Flask, jsonify #jsonify formats the output properly. ie, it returns a valid json
 import requests
 
 app = Flask(__name__)
@@ -11,6 +11,17 @@ def hello_world():
 @app.route("/resource/<string:n>")
 def particular_rep(n):
     response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{n}')
+    return jsonify(response.json())
+
+@app.route("/resource/<string:n>/comments")
+def comments(n):
+    response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{n}/comments')
+    return jsonify(response.json())
+
+@app.route("/resource/comments?postId=<string:n>")
+def comments_2(n):
+    url_params = {'postId': n}
+    response = requests.get(f'https://jsonplaceholder.typicode.com/comments',params=url_params)
     return jsonify(response.json())
 
 if __name__ == "__main__":
